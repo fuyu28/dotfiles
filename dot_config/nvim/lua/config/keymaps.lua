cc063c16b370b0ed155e132778814a3e8fe2b36a
fuyu <88916226+fuyu28@users.noreply.github.com>
2026-06-18 16:50:18 +0900

    feat: nvimをデフォルトでブラックホールレジスタに設定

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- ===== デフォルトは全部ブラックホール =====
+map({ "n", "x" }, "d", '"_d')
+map({ "n", "x" }, "D", '"_D')
+map({ "n", "x" }, "c", '"_c')
+map({ "n", "x" }, "C", '"_C')
+map({ "n", "x" }, "x", '"_x')
+map("x", "p", '"_dP') -- ビジュアルでペーストしてもヤンクが残る
+
+-- ===== 明示的に「切り取ってレジスタに入れる」= <leader>d =====
+-- noremap なので右辺の d は“組み込みの d”＝本物のカット
+map({ "n", "x" }, "<leader>d", "d", { desc = "Cut (yank + delete)" })
+map("n", "<leader>D", "dd", { desc = "Cut line" })
